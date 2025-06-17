11daf0ace400199bfa750440ac20497360804ee8
Nato0211 <nato0211@gmail.com>
2025-06-16 20:51:39 -0500

    feat: creation of packages

---
+++ src/test/java/com/healthlifei/u20221b471/main/medicalhistoryservice/MedicalHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthlifei.u20221b471.main.medicalhistoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalHistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
